1e9e4f0cd3b43aa574a512d8d56dfb3b9b49de77
DavidDworetzky <djdworetz@gmail.com>
2021-04-13 00:25:37 -0500

    Refactored AppSentinel to use settings manager

--- AppSentinel/AppSentinel.cs
+++ AppSentinel/AppSentinel.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AppSentinel.Core.Managers;
 
 namespace AppSentinel
 {
@@ -16,66 +17,60 @@ namespace AppSentinel
         #region TriggerConstants
         private const string Urls = "TriggerUrls";
         private const string Triggers = "TriggerValues";
-        private const string Heartbeat = "TriggerHeartbeat";
 
         private const string SendGridKey = "SendGridKey";
         private const string SendGridFromEmail = "SendGridFromEmail";
         private const string SendGridFrom = "SendGridFrom";
-        private const string SendGridTargets = "SendGridTo";
+        private const string SendGridTargets = "SendGridTargets";
 
-        private const string HeartbeatEnabled = "AppSentinelHeartbeat";
+        private const string HeartbeatEnabled = "HeartbeatEnabled";
 
         #endregion
+
+        private static Dictionary<string, string> GetSettingsValues()
+        {
+            Func<string, string> getSettings = (str) => System.Environment.GetEnvironmentVariable(str, EnvironmentVariableTarget.Process);
+            return new Dictionary<string, string>()
+            {
+                {"TriggerUrls", getSettings(Urls) },
+                {"TriggerValues", getSettings(Triggers) },
+                {"SendGridKey", getSettings(SendGridKey) },
+                {"SendGridFrom", getSettings(SendGridFrom) },
+                {"SendGridTargets", getSettings(SendGridTargets) },
+                {"SendGridFromEmail", getSettings(SendGridFromEmail) }
+            };
+        }
 
         [FunctionName("AppSentinel")]
         public static async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
             //deserialize settings for urls and triggers for our WebAlertGenerator.
-            //url
-            var urls = System.Environment.GetEnvironmentVariable(Urls, EnvironmentVariableTarget.Process);
-            var splitUrls = urls.Split(',');
-            //webalerttriggers
-            var triggers = System.Environment.GetEnvironmentVariable(Triggers, EnvironmentVariableTarget.Process);
-            var splitTriggers = triggers.Split(';');
-            //now, split again on comma separation and create WebAlertTrigger
-            //alert format is:
-            //200,299,true,,HttpFailure
-            var webAlerts = splitTriggers.Select((val) =>
-            {
-                var splitValues = val.Split(',');
-                var statusRange1 = Int32.Parse(splitValues[0]);
-                var statusRange2 = Int32.Parse(splitValues[1]);
-                var statusRange = new List<int>() { statusRange1, statusRange2 };
-                var checkSuccessful = bool.Parse(splitValues[2]);
-                var regex = splitValues[3];
-                Core.Models.WebAlertType webAlertType;
-                var webAlertTypeParse = Enum.TryParse<Core.Models.WebAlertType>(splitValues[4], out webAlertType);
-                var webAlert = new Core.Models.WebAlertTrigger(statusRange, checkSuccessful, regex, webAlertType);
-                return webAlert;
-            }).ToList();
+            var settings = GetSettingsValues();
+            var settingsManager = new WebAlertSettingsManager(string.Empty);
+            var sentinelSettings = settingsManager.LoadSettingsFromDictionary(settings);
+
             //generate alerts
-            var webAlertGenerator = new Core.Managers.WebAlertGenerator(splitUrls.ToList(), webAlerts);
+            var webAlertGenerator = new Core.Managers.WebAlertGenerator(sentinelSettings.WebAlertSettings.Urls.ToList(), sentinelSettings.WebAlertSettings.Triggers.ToList());
             var alerts = webAlertGenerator.GenerateAlerts() as IEnumerable<Core.Models.WebAlert>;
 
-            //get sendgrid environment variables
-            var sendGridKey = System.Environment.GetEnvironmentVariable(SendGridKey, EnvironmentVariableTarget.Process);
-            var sendGridFromEmail = System.Environment.GetEnvironmentVariable(SendGridFromEmail, EnvironmentVariableTarget.Process);
-            var sendGridFrom = System.Environment.GetEnvironmentVariable(SendGridFrom, EnvironmentVariableTarget.Process);
-            var sendGridTargets = System.Environment.GetEnvironmentVariable(SendGridTargets, EnvironmentVariableTarget.Process).Split(',').ToList();
+            var alertSettings = sentinelSettings.WebAlertSettings;
+            var notificationSettings = sentinelSettings.NotificationSettings;
+
             //notify alerts via EmailNotificationManager
-            var emailNotificationManager = new Core.Managers.EmailNotificationManager(sendGridKey, sendGridFrom, sendGridFromEmail);
+            var emailNotificationManager = new Core.Managers.EmailNotificationManager(notificationSettings.SendGridApiKey, notificationSettings.FromEmail, notificationSettings.From);
             foreach(var alert in alerts)
             {
-                await emailNotificationManager.NotifyMessageToTargets(sendGridTargets, alert.Description, $"<strong>{alert.Description}</strong>", $"App Sentinel Alert from {sendGridFrom}");
+                await emailNotificationManager.NotifyMessageToTargets(notificationSettings.SendGridTargets.ToList(), alert.Description, $"<strong>{alert.Description}</strong>", $"App Sentinel Alert from {notificationSettings.From}");
             }
 
             //heartbeat
             var hearbeat = System.Environment.GetEnvironmentVariable(HeartbeatEnabled, EnvironmentVariableTarget.Process);
-            var doHeartbeat = Boolean.Parse(Heartbeat);
+            var doHeartbeat = Boolean.Parse(hearbeat);
             if(doHeartbeat)
             {
-                var heartbeatContent = $"AppSentinel checked URLs for errors at {DateTime.Now}. Checked URLs: {urls}";
-                await emailNotificationManager.NotifyMessageToTargets(sendGridTargets, heartbeatContent, $"<strong>{heartbeatContent}</strong>", "AppSentinel Check Notification");
+                var urlAggregate = sentinelSettings.WebAlertSettings.Urls.Aggregate("", (x, y) => x +":" + y, (result) => result);
+                var heartbeatContent = $"AppSentinel checked URLs for errors at {DateTime.Now}. Checked URLs: {urlAggregate}";
+                await emailNotificationManager.NotifyMessageToTargets(notificationSettings.SendGridTargets.ToList(), heartbeatContent, $"<strong>{heartbeatContent}</strong>", "AppSentinel Check Notification");
             }
 
 
